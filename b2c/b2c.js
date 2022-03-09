c7015ba9e1e7effec22d55b928fafe4e44b65aa3
Kai <thecodedmind@gmx.co.uk>
2022-03-09 20:14:50 +0000

    Sync

--- b2c/b2c.js
+++ b2c/b2c.js
@@ -1,6 +1,8 @@
 // Create WebSocket connection.
 const socket = new WebSocket('ws://82.6.205.72:7790');
 let seen = [];
+let users = {}
+let my_id = null;
 
 // Connection opened
 socket.addEventListener('open', function (event) {
@@ -9,14 +11,26 @@ socket.addEventListener('open', function (event) {
 
 function add_msg(text){
     let v = document.getElementById('results').innerHTML;
+    let dt = new Date().toLocaleString();
+    text = `[${dt}] ${text}`
     v = text + "<br> "+ v;
     document.getElementById('results').innerHTML = v;
+
 }
 
 function update_seen(){
     document.getElementById('users').innerHTML = seen.join("<br>");
 }
 
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+  
 // Listen for messages
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
@@ -28,21 +42,49 @@ socket.addEventListener('message', function (event) {
         add_msg(`${sender}: ${line}`);
         if(!seen.includes(sender)) seen.push(sender);
         update_seen();
+    } else if(evt == "ID"){
+        my_id = event.data.split("/")[2];
+        console.log(`ID defined ${my_id}`)
+    } else if(evt == "TEST"){
+        seen.push(event.data.split("/")[3]);
+        update_seen();
+        users[event.data.split("/")[2]] = event.data.split("/")[3];
+        add_msg(`${event.data.split("/")[3]} joined the chat.`)
+    } else if(evt == "DISCONNECT"){
+        add_msg(`${users[event.data.split("/")[2]]} left the chat.`)
+        removeItemOnce(seen, users[event.data.split("/")[2]]);
+        users[event.data.split("/")[2]] = null;
+        //add_msg(`${}`)
+        
+        update_seen()
     } else {
         let line = event.data.split("/")[2];
-        add_msg(evt+" "+line)
+        console.log(evt+" "+line);
     }
 
 });
 
 window.onload = function() {
+    document.getElementById('ftext').disabled = true;
     document.getElementById('ftext').addEventListener('keyup',function(e){
         if (e.keyCode === 13) {
-        sendChat();
+            sendChat();
       }
     });
 }
 
+function doConnect(){
+    let name = document.getElementById('fname').value;
+    if(name == ""){
+        add_msg("Name is needed...");
+        return
+    }
+    console.log(my_id);
+    console.log(name);
+    socket.send("/TEST/"+my_id+"/"+name+"/");
+    document.getElementById('fname').disabled = true;
+    document.getElementById('ftext').disabled = false;
+} 
 
 function sendChat(){
     let name = document.getElementById('fname').value;
--- b2c/b2c_server.py
+++ b2c/b2c_server.py
@@ -1,56 +1,86 @@
 #!/usr/bin/env python3
+# Import Python's asyncio library to enable concurrency
 import asyncio
-import enum
+# Import websockets, the protocol we're using for the connection 
 import websockets
+# Import bashlib, for base level encryption on stored passwords
 import hashlib
 
+# Defining a dictionary where we will store references to each client that connects.
 clients = {}
 
- 
+# Return a hash of a given text value, e.g. an encrypted string that can't be converted back to original.
 def hash(msg):
     return hashlib.sha256(bytes(msg, "utf-8")).hexdigest()
 
+# Event for when a client connects.
 async def on_connect(ws):
+    # Show in server log that a client has connected.
     print(f"{ws.local_address} {ws.id} connected.")
+    
+    # Store a reference to the socket connection object in our clients dictionary
     clients[str(ws.id)] = ws
+
+    # Send the connected client their unique ID that the client will use to identify itself
+    await ws.send(f"/ID/{ws.id}/")
+
+    # Tell all clients that this client has connected
     for id, client in clients.items():
         await client.send(f"/CONNECT/{ws.id}/")
 
     
-
+# Event for when a client disconnects.
 async def on_disconnect(ws, error = False):
     print(f"{ws.local_address} disconnected ({error}).")
+
+    # Remove this client from the dict
     if clients.get(str(ws.id), None) != None:
         del clients[str(ws.id)] 
+
+    # Announce the departure
     for id, client in clients.items():
         await client.send(f"/DISCONNECT/{ws.id}/");
 
+# Event for recieving a message from the client
 async def on_message(ws, msg):
-    print(f"recv {ws.id}: {msg}")
+    print(f"MESSAGE {ws.id}: {msg}")
+
+    # Dispatch message to all clients except sender
     for id, client in clients.items():
         if ws != client: await client.send(msg)
 
+# Main loop of the server
 async def client_loop(websocket):
+    # Define an error check variable
     error = False
+
+    # Run our connect event
     await on_connect(websocket)
 
+    # The script halts here, and waits for messages until they disconnect
     try:
+        # Async, allowing this loop to run without interrupting other loops also running
+        # Essentially allowing multiple copies of this to run at the same time, one for each client.
         async for message in websocket:
+            # Send it to our message event.
             await on_message(websocket, message)
+
+            # Echo it back to the sender
             await websocket.send(message)
     except websockets.exceptions.ConnectionClosedError:
+        # We've errored, so set this to true
         error = True
 
+    # Once the client disconnects, the previous loop ends, allowing this function to now run
     await on_disconnect(websocket, error)
-import ssl, pathlib
 
 async def main():
-    """ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-
-    localhost_pem = pathlib.Path(__file__).with_name("snakeoil.pem")
-    key_pem = pathlib.Path(__file__).with_name("snakeoil.key")
-    ssl_context.load_cert_chain(localhost_pem, key_pem)"""
-    async with websockets.serve(client_loop, "", 7790):#, ssl=ssl_context):
+    # Starting our server.
+    # Parameter 1: the function to use for the client that connects
+    # Parameter 2: leave blank
+    # Parameter 3: the servers port
+    
+    async with websockets.serve(client_loop, "", 7790):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
