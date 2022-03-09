7aea1e5606f084ad07b769a8eb540deccc5963df
Kai <thecodedmind@gmx.co.uk>
2022-03-09 12:52:13 +0000

    update comments

--- passwordgenerator/pwdgentk.py
+++ passwordgenerator/pwdgentk.py
@@ -1,18 +1,25 @@
-# Import tkinter, our graphics library
+# Import everything from tkinter, our graphics library
+# importing * takes everything from the tkinter module and stores them in our script
 from tkinter import *
+# Import the ttk object from GUI library, which provides some extra functionality
+# importing this object creates a variable ttk in this script that we can use to access it
 from tkinter import ttk
 
-# Import Python's Random module
+# Import Python's Random module, which provides the functionality we will use to generate randomness.
 import random
 
 # Constants
-chars = list("abcdefghijklmonpqrstuvqyz") # List function splits a string by each character.
-chars_upper = list("ABCDEFGHIJKLMONPQRSTUVWXYZ")
-nums = list(range(0, 10)) # List can also convert a range, which will create a list of numbers 0 to 9
-syms = list("!\"Â£$%^&*()_+-=[]{};':@',./<>?")
+# Python itself doesn't have the concept of constants natively
+# But common practice is to identify variables that should not be changed as capitalised
+LETTERS = list("abcdefghijklmonpqrstuvqyz") # List function splits a string by each character.
+UPPERCASE_LETTERS = list("ABCDEFGHIJKLMONPQRSTUVWXYZ")
+NUMBERS = list(range(0, 10)) # List can also convert a range, which will create a list of numbers 0 to 9
+SYMBOLS = list("!\"Â£$%^&*()_+-=[]{};':@',./<>?")
 
 # Variables
-options = [chars, chars_upper, nums, syms] # List of list,s listception!
+# My methodology here is, storing the lists within a list lets me easily randomly select each character
+# by first selecting the type from this list, selecting the character from the list we've just selected
+options = [LETTERS, UPPERCASE_LETTERS, NUMBERS, SYMBOLS] # List of lists, listception!
 
 # Defining our input and output boxes
 # We're creating these here, even if the elements themselves are not being created yet, so the entire script can access them.
@@ -27,13 +34,15 @@ root = Tk()
 # Give our window a title
 root.title("pwdgen")
 
-# Functions
+# def is short for define; this statement defines a new function called generate, which takes a value called max_len when called
 def generate(max_len):
+    # Everything indented to this level is part of this function
+
     # Create the container for our password we're making
     out = ""
 
     # Loop while length of output is under our desired maximum
-    while len(out) <= max_len:
+    while len(out) < max_len:
         # Choose the type of character, out of the options list we defined earlier
         type_of_char = random.choice(options)
 
@@ -48,10 +57,12 @@ def clicked_button():
     # Clear our computers clipboard
     root.clipboard_clear()
 
-    # Get the text from our input box
+    # Get the text from our input box and save it as variable named l
     l = pwdlen_input.get()
 
     # Checking if input is a valid number.
+    # isdigit returns true if l is only a number.
+    # `if not` checks if the following statement is false, then executes the indented block if it is.
     if not l.isdigit():
         # This section executes if variable l is not a number.
 
@@ -92,6 +103,8 @@ def modifier(m):
            
     if m == "-": new_len = int(l) - 1
 
+    # SANITY CHECK: If number is 0, set to 1
+    if new_len == 0: new_len = 1
     # Overwrite the input box value
     pwdlen_input.delete(0, END)
     pwdlen_input.insert(0, str(new_len))
