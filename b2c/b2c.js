589cb04aee5f7473951ddd6ee06b94650e35e100
Kai <thecodedmind@gmx.co.uk>
2022-03-09 16:50:54 +0000

    files

--- b2c/b2c.js
+++ b2c/b2c.js
@@ -9,4 +9,14 @@ socket.addEventListener('open', function (event) {
 // Listen for messages
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
+    let v = document.getElementById('results').innerHTML;
+    v = event.data + "<br> "+ v;
+    document.getElementById('results').innerHTML = v;
 });
+
+function sendChat(){
+    let v = document.getElementById('fname').value;
+
+    console.log(v);
+    socket.send(v);
+} 
